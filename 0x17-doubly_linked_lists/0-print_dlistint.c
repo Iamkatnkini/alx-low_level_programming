fcda21c25e04d38b4b42f87f6e583a25d622479a
Katherin Nkini <katnkinincheku@gmail.com>
2023-06-17 08:35:51 -0700

    #include lists.h
    /**
     * print_dlistint - prints data in nodes
     * @h: head of node
     * Return: how many nodes
     */
    size_t print_dlistint(const dlistint_t *h)
    {
    size_t count = 0;
    
    while (h != NULL)
    {
    printf(%dn, h->n);
    h = h->next;
    count++;
    }
    
    return (count);

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
+ * print_dlistint - prints data in nodes
+ * @h: head of node
+ * Return: how many nodes
  */
-size_t print_dlistint_t(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		h = h->next;
-		s++;
+		count++;
 	}
 
-	return (s);
+	return (count);
 }
-
